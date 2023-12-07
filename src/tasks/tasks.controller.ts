6363bf99a09c4f288875fd2f9f7e557db063850c
Muhammad Ghifar <muhammadghifarrijali@gmail.com>
2023-12-07 11:40:43 +0700

    add createTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -10,5 +10,13 @@ export class TasksController {
   // penamaan fungsi bebas, tidak harus sama dengan di service
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
