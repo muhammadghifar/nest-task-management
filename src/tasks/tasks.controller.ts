f54dd7c90257004c06961a39a7c6aeaa1b726540
Muhammad Ghifar <muhammadghifarrijali@gmail.com>
2023-12-07 11:06:56 +0700

    getalltasks & add model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  // penamaan fungsi bebas, tidak harus sama dengan di service
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
 }
