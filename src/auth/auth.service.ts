9a0ab23072f73c37bd05c01dfcf32dc75cac076a
Muhammad Ghifar <muhammadghifarrijali@gmail.com>
2023-12-09 13:05:39 +0700

    init auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  // async signIn(authCredentialsDto: AuthCredentialsDto): Promise<> {
+  //   const { username } = authCredentialsDto;
+
+  //   return await this.usersRepository.findOne({ username });
+  // }
+}
---
+++ src/auth/users.repository.ts
@@ -0,0 +1,26 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from './user.entity';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      }
+
+      throw new InternalServerErrorException();
+    }
+  }
+}
