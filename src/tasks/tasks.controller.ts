37026210f36630e3f3159f6d133c26198bc1aac7
Muhammad Ghifar <muhammadghifarrijali@gmail.com>
2024-01-08 14:56:26 +0700

    config n environment

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -18,13 +18,20 @@ import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/auth/user.entity';
 import { GetUser } from './get-user.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
   private logger = new Logger('TasksController');
 
-  constructor(private tasksService: TasksService) {}
+  constructor(
+    private tasksService: TasksService,
+    private configService: ConfigService,
+  ) {
+    // TEST_VALUE adalah env
+    console.log(configService.get('TEST_VALUE'));
+  }
 
   @Get()
   getTasks(
